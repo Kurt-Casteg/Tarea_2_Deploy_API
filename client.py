e7069918ae3f0e297389177356cff187ba187368
Kurt-Casteg <k.castroo@udd.cl>
2025-08-16 15:17:00 -0400

    Actualización de la URL de la API en 'client.py' para apuntar a localhost y habilitación del despliegue automático en 'render.yaml'. Se realizaron cambios en 'requirements.txt'.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 import time
 
-API_URL = "https://tarea-2-deploy-api.onrender.com"  # Cambiar por URL de Render
+API_URL = "http://localhost:8000"  # Cambiar por URL de Render
 
 def print_separator():
     print("=" * 70)
